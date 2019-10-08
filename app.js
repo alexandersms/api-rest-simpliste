a1c1ac193f82f57c4e40ecd1b58438816e3771bf
Alex Simisi <smsxander@gmail.com>
2019-10-09 00:50:28 +0200

    installation du middleware Morgan

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 require("babel-register");
-var express = require("express");
-var app = express();
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/api", (req, res) => {
+  res.send("Root API");
+});
+
+app.get("/api/v1", (req, res) => {
+  res.send("API version 1");
+});
+
+app.get("/api/v1/books/:id", (req, res) => {
+  res.send(req.params);
+});
+
+app.listen(8080, () => console.log("Started on port 8080."));
